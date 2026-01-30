30a35a2f7b4f89d1631506ad4de6e45f9a6573da
Oded Shahar <oded.shahar@gmail.com>
2026-01-30 12:09:31 -0800

    Use 'hf' command for HF downloads and fix Ollama storage path in Cloud Build

--- src/cr_infer/models.py
+++ src/cr_infer/models.py
@@ -161,7 +161,7 @@ subprocess.run(['gsutil', 'cp', metadata_file, f'gs://{bucket}/{metadata_file}']
                 "entrypoint": "bash",
                 "args": [
                     "-c",
-                    "pip install huggingface_hub && hash -r && huggingface-cli download $_MODEL_ID --local-dir /workspace/model-repo --token $_HF_TOKEN"
+                    "pip install huggingface_hub && hf download $_MODEL_ID --local-dir /workspace/model-repo --token $_HF_TOKEN"
                 ],
                 "id": "download_model_repo"
             },
@@ -185,7 +185,7 @@ subprocess.run(['gsutil', 'cp', metadata_file, f'gs://{bucket}/{metadata_file}']
             },
             {
                 "name": "gcr.io/cloud-builders/gsutil",
-                "args": ["-m", "cp", "-r", "/builder/home/.ollama/*", "gs://$_BUCKET_NAME/ollama/"],
+                "args": ["-m", "cp", "-r", "/root/.ollama/*", "gs://$_BUCKET_NAME/ollama/"],
                 "id": "upload_to_gcs"
             },
             *common_final_steps

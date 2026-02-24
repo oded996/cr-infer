f26c605f826f324def0ff434280260035b67e836
Oded Shahar <oded.shahar@gmail.com>
2026-02-24 14:40:15 -0800

    fix: correct entry point in setup.py from app to cli

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "cr-infer=cr_infer.cli.main:app",
+            "cr-infer=cr_infer.cli.main:cli",
         ],
     },
 )
